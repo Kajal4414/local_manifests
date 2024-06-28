2b2de4c41edc283212e565c24278d38014fcd614
kajal4414 <81718060+kajal4414@users.noreply.github.com>
2024-06-29 03:14:43 +0530

    drop miuicamera `not work`

--- crv.sh
+++ crv.sh
@@ -9,7 +9,6 @@ rm -rf hardware/google/pixel/kernel_headers
 git clone -b 13.0 --depth=1 https://github.com/kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes
 git clone -b 13.0 --depth=1 https://github.com/kajal4414/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
 git clone -b 13.0 --depth=1 https://github.com/kajal4414/android_kernel_xiaomi_spes.git kernel/xiaomi/spes
-git clone -b 13.0 --depth=1 https://gitlab.com/kajal4414/vendor-xiaomi-camera.git vendor/xiaomi/camera
 git clone -b lineage-20 --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
 git clone -b lineage-20 --depth=1 https://github.com/kajal4414/android_hardware_lineage_compat.git hardware/lineage/compat
 
