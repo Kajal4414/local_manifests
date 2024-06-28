bb997fd1a5c1d017ad019f9be276e00bea266039
kajal4414 <81718060+kajal4414@users.noreply.github.com>
2024-06-28 19:50:40 +0530

    Update crv.sh

--- crv.sh
+++ crv.sh
@@ -1,12 +1,11 @@
-rm -rf .repo/local_manifest
 repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs
 /opt/crave/resync.sh
 
-rm -rf */xiaomi # deletes all 'xiaomi' directories recursively
+rm -rf */xiaomi
 rm -rf hardware/lineage/compat
 rm -rf hardware/google/pixel/kernel_headers
 
-git clone -b 13.0 --depth=1 https://github.com/kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes && rm -rf device/xiaomi/spes/vendorsetup.sh
+git clone -b 13.0 --depth=1 https://github.com/kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes
 git clone -b 13.0 --depth=1 https://github.com/kajal4414/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
 git clone -b 13.0 --depth=1 https://github.com/kajal4414/android_kernel_xiaomi_spes.git kernel/xiaomi/spes
 git clone -b 13.0 --depth=1 https://gitlab.com/kajal4414/vendor-xiaomi-camera.git vendor/xiaomi/camera
@@ -15,5 +14,5 @@ git clone -b lineage-20 --depth=1 https://github.com/kajal4414/android_hardware_
 
 . build/envsetup.sh
 lunch lineage_spes-user
-make installclean # make clean
+make installclean
 m bacon
