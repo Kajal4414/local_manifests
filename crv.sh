78daa0bdcb4103e37266514160434fcb01391bae
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-01 23:00:41 +0530

    Update crv.sh

--- crv.sh
+++ crv.sh
@@ -11,9 +11,6 @@ git clone -b 13.0 --depth=1 https://github.com/Kajal4414/android_vendor_xiaomi_s
 git clone -b 13.0 --depth=1 https://github.com/Kajal4414/android_kernel_xiaomi_spes.git kernel/xiaomi/spes
 git clone -b lineage-20 --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
 git clone -b lineage-20 --depth=1 https://github.com/Kajal4414/android_hardware_lineage_compat.git hardware/lineage/compat
-git config --global user.name Kajal4414
-git config --global user.email 81718060+Kajal4414@users.noreply.github.com
-(cd packages/apps/WallpaperPicker2 && git fetch https://github.com/RisingTechOSS/android_packages_apps_WallpaperPicker2.git && git cherry-pick e0bc414^..4c16373)
 
 . build/envsetup.sh
 lunch lineage_spes-user
