1e0e3a672ebd457f22c82c4b19c97da67e1b4df0
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-08 16:17:04 +0530

    Build `crDroid`
    Signed-off-by: Kajal4414 <81718060+Kajal4414@users.noreply.github.com>

--- crv.sh
+++ crv.sh
@@ -1,15 +1,16 @@
 rm -rf .repo/local_manifest
+repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs
 /opt/crave/resync.sh
 
 rm -rf */xiaomi
 rm -rf hardware/lineage/compat
 rm -rf hardware/google/pixel/kernel_headers out
 
-git clone -b 13.0 --depth=1 https://github.com/Kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes
-git clone -b 13.0 --depth=1 https://github.com/Kajal4414/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
-git clone -b 13.0 --depth=1 https://github.com/Kajal4414/android_kernel_xiaomi_spes.git kernel/xiaomi/spes
-git clone -b lineage-20 --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
-git clone -b lineage-20 --depth=1 https://github.com/Kajal4414/android_hardware_lineage_compat.git hardware/lineage/compat
+git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes
+git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
+git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_kernel_xiaomi_spes.git kernel/xiaomi/spes
+git clone -b lineage-20 --depth 1 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+git clone -b lineage-20 --depth 1 https://github.com/Kajal4414/android_hardware_lineage_compat.git hardware/lineage/compat
 
 . build/envsetup.sh
 lunch lineage_spes-user
