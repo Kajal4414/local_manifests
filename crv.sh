6636f252372e0437d98dad7a3b6ba0497a8036e0
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-11 22:02:18 +0530

    crv: update lineage fork branch
    Change-Id: Ic4505adadc469a3dd06e6e100732de326da45091
    Signed-off-by: Kajal4414 <81718060+Kajal4414@users.noreply.github.com>

--- crv.sh
+++ crv.sh
@@ -8,8 +8,8 @@ rm -rf hardware/google/pixel/kernel_headers out
 git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes
 git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
 git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_kernel_xiaomi_spes.git kernel/xiaomi/spes
-git clone -b lineage-20 --depth 1 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
-git clone -b lineage-20 --depth 1 https://github.com/Kajal4414/android_hardware_lineage_compat.git hardware/lineage/compat
+git clone -b lineage-20.0 --depth 1 https://github.com/Kajal4414/android_hardware_xiaomi.git hardware/xiaomi
+git clone -b lineage-20.0 --depth 1 https://github.com/Kajal4414/android_hardware_lineage_compat.git hardware/lineage/compat
 
 if ! grep -q "CR_VERSION" "vendor/lineage/config/version.mk"; then
     rm -rf device/xiaomi/spes/sepolicy/private/recovery.te
