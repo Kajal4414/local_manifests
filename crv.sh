75aef8de11a6e495d086a7d73672998a90aaf937
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-15 09:25:21 +0530

    Update crv.sh

--- crv.sh
+++ crv.sh
@@ -5,7 +5,7 @@ rm -rf */xiaomi
 rm -rf hardware/lineage/compat
 rm -rf hardware/google/pixel/kernel_headers out
 
-git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes
+git clone -b rebase --depth 1 https://github.com/Kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes
 git clone -b 13.0-dev --depth 1 https://github.com/Kajal4414/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
 git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_kernel_xiaomi_spes.git kernel/xiaomi/spes
 git clone -b lineage-20.0 --depth 1 https://github.com/Kajal4414/android_hardware_xiaomi.git hardware/xiaomi
