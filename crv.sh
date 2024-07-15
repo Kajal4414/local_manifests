c92bac462316152933c6e331a35e8a5a0304f661
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-16 03:44:05 +0530

    crv: crdroid

--- crv.sh
+++ crv.sh
@@ -1,12 +1,13 @@
 rm -rf .repo/local_manifest
+repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs
 /opt/crave/resync.sh
 
 rm -rf */xiaomi
 rm -rf hardware/lineage/compat
 rm -rf hardware/google/pixel/kernel_headers out
 
-git clone -b rebase --depth 1 https://github.com/Kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes
-git clone -b 13.0-dev --depth 1 https://github.com/Kajal4414/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
+git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes
+git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
 git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_kernel_xiaomi_spes.git kernel/xiaomi/spes
 git clone -b lineage-20.0 --depth 1 https://github.com/Kajal4414/android_hardware_xiaomi.git hardware/xiaomi
 git clone -b lineage-20.0 --depth 1 https://github.com/Kajal4414/android_hardware_lineage_compat.git hardware/lineage/compat
