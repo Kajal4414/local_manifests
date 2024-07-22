5eb3e65fdc702a1736f366cdeffd665e6bfbdbfc
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-22 22:52:42 +0530

    Update crv.sh

--- crv.sh
+++ crv.sh
@@ -6,15 +6,15 @@ rm -rf */xiaomi
 rm -rf hardware/lineage/compat
 rm -rf hardware/google/pixel/kernel_headers out
 
-git clone -b 13.0 https://github.com/Kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes
+git clone -b 13.0-new https://github.com/Kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes
 git clone -b 13.0-new https://github.com/Kajal4414/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
 git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_kernel_xiaomi_spes.git kernel/xiaomi/spes
 git clone -b lineage-20.0 --depth 1 https://github.com/Kajal4414/android_hardware_xiaomi.git hardware/xiaomi
 git clone -b lineage-20.0 --depth 1 https://github.com/Kajal4414/android_hardware_lineage_compat.git hardware/lineage/compat
 
 git config --global user.name "Kajal4414" && git config --global user.email "81718060+Kajal4414@users.noreply.github.com"
-(cd device/xiaomi/spes && git pull && git revert -s acc89bf --no-edit) # spes: props: Sync properties with miui
-(cd vendor/xiaomi/spes && git pull && git reset --hard HEAD~1) # spes: Update blobs from SPESINGlobal_OS1.0.2.0.TGCINXM
+(cd device/xiaomi/spes && git pull && git reset --hard HEAD~1)
+# (cd vendor/xiaomi/spes && git pull && git reset --hard HEAD~1)
 
 . build/envsetup.sh
 lunch lineage_spes-user
