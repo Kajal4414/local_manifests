4b35987b04a3d6aebd771178e6c855f075a270c9
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-23 10:57:54 +0530

    test `skiaglthreaded` to `remove kernel flags` and `uvite` kernel

--- crv.sh
+++ crv.sh
@@ -6,14 +6,14 @@ rm -rf */xiaomi
 rm -rf hardware/lineage/compat
 rm -rf hardware/google/pixel/kernel_headers out
 
-git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes
-git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
-git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_kernel_xiaomi_spes.git kernel/xiaomi/spes
+git clone -b 13.0 https://github.com/Kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes
+git clone -b 13.0 https://github.com/Kajal4414/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
+git clone -b uvite --depth 1 https://github.com/CHRISL7/kernel_xiaomi_sm6225.git kernel/xiaomi/spes
 git clone -b lineage-20.0 --depth 1 https://github.com/Kajal4414/android_hardware_xiaomi.git hardware/xiaomi
 git clone -b lineage-20.0 --depth 1 https://github.com/Kajal4414/android_hardware_lineage_compat.git hardware/lineage/compat
 
-# git config --global user.name "Kajal4414" && git config --global user.email "81718060+Kajal4414@users.noreply.github.com"
-# (cd device/xiaomi/spes && git pull && git reset --hard HEAD~1)
+git config --global user.name "Kajal4414" && git config --global user.email "81718060+Kajal4414@users.noreply.github.com"
+(cd device/xiaomi/spes && git pull && git cherry-pick eb06bec 6b49f66 33faf83 b94a188) # skiaglthreaded to remove kernel flags
 # (cd vendor/xiaomi/spes && git pull && git reset --hard HEAD~1)
 
 . build/envsetup.sh
