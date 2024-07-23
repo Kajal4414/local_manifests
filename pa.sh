8965967c8af6e34ef69ffeb98805da0eb3c03b19
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-24 01:20:41 +0530

    Update and rename aospa.sh to pa.sh

--- pa.sh
+++ pa.sh
@@ -2,4 +2,4 @@ rm -rf .repo/local_manifest out
 repo init -u https://github.com/AOSPA/manifest -b uvite
 /opt/crave/resync.sh
 
-./rom-build.sh spes -i -t user
+./rom-build.sh spes -i -v beta -t user
