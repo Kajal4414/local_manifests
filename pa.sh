b6fa4f1a42ee7d8223c728ecbd9ee9ce91f1be12
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-24 21:39:49 +0530

    pa: drop signed build
    Signed-off-by: Kajal4414 <81718060+Kajal4414@users.noreply.github.com>

--- pa.sh
+++ pa.sh
@@ -1,21 +1,8 @@
-rm -rf .repo/local_manifest out
-repo init -u https://github.com/AOSPA/manifest -b uvite
-/opt/crave/resync.sh
-
-export KEY_MAPPINGS=vendor/aospa/signatures
 export AOSPA_BUILDTYPE=BETA
 
-git config --global user.name "Kajal4414"
-git config --global user.email "81718060+Kajal4414@users.noreply.github.com"
-(cd build/soong && git fetch https://gerrit.aospa.co/AOSPA/android_build_soong refs/changes/57/40257/1 && git cherry-pick FETCH_HEAD)
-git clone https://github.com/aospa-chrisl7/android_vendor_aospa_signatures.git vendor/aospa/signatures
-(cd vendor/aospa/signatures && ./generate.sh)
+rm -rf .repo/local_manifest out && repo init -u https://github.com/AOSPA/manifest -b uvite && /opt/crave/resync.sh
 
-m otatools target-files-package -j$(nproc)
-sign_target_files_apks -o -d $KEY_MAPPINGS $OUT/obj/PACKAGING/target_files_intermediates/aospa_spes-target_files-eng.nobody.zip aospa-user-signed-target_files-eng.nobody.zip
-ota_from_target_files -k $KEY_MAPPINGS/releasekey --block aospa-user-signed-target_files-eng.nobody.zip aospa-user.zip
+git clone -b uvite https://github.com/Kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes
+git clone -b uvite https://github.com/Kajal4414/android_device_xiaomi_sm6225-common.git device/xiaomi/sm6225-common
 
-. build/envsetup.sh
-lunch aospa_spes-user
-m installclean -j$(nproc)
-m otapackage -j$(nproc)
+source build/envsetup.sh && lunch aospa_spes-user && m clobber -j$(nproc) && m otapackage -j$(nproc)
