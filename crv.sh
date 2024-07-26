7022c46d6a6b3f1f71d55cd06291e2c92f0f10b8
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-26 23:27:24 +0530

    crv: update los branch

--- crv.sh
+++ crv.sh
@@ -9,8 +9,8 @@ rm -rf hardware/google/pixel/kernel_headers out
 git clone -b 13.0 https://github.com/Kajal4414/android_device_xiaomi_spes.git device/xiaomi/spes
 git clone -b 13.0 https://github.com/Kajal4414/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
 git clone -b 13.0 --depth 1 https://github.com/Kajal4414/kernel_xiaomi_sm6225.git kernel/xiaomi/spes
-git clone -b lineage-20.0 --depth 1 https://github.com/Kajal4414/android_hardware_xiaomi.git hardware/xiaomi
-git clone -b lineage-20.0 --depth 1 https://github.com/Kajal4414/android_hardware_lineage_compat.git hardware/lineage/compat
+git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_hardware_xiaomi.git hardware/xiaomi
+git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_hardware_lineage_compat.git hardware/lineage/compat
 
 git config --global user.name "Kajal4414" && git config --global user.email "81718060+Kajal4414@users.noreply.github.com"
 (cd device/xiaomi/spes && git pull && git cherry-pick eb06bec^..52a728e) # skiaglthreaded to reorganize files
