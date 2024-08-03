e986098a1f2bd386a354c0509e05fa48ca448033
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-08-04 03:02:41 +0530

    crv: hmmm

--- crv.sh
+++ crv.sh
@@ -15,6 +15,7 @@ git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_hardware_lineag
 git config --global user.name "Kajal4414" && git config --global user.email "81718060+Kajal4414@users.noreply.github.com"
 (cd packages/apps/crDroidSettings && git fetch https://github.com/Kajal4414/android_packages_apps_crDroidSettings.git && git cherry-pick 252a9a9 428bf1e) # remove game space shortcut, updater
 (cd frameworks/base && git fetch https://github.com/Kajal4414/android_frameworks_base.git && git cherry-pick d264ce2) # remove support for game space
+(cd vendor/lineage && git fetch https://github.com/Kajal4414/android_vendor_crdroid.git && git cherry-pick 0e584f7) # remove some packages
 
 # (cd device/xiaomi/spes && git pull && git cherry-pick eb06bec^..52a728e) # skiaglthreaded to reorganize files
 # (cd vendor/xiaomi/spes && git pull && git reset --hard HEAD~1)
