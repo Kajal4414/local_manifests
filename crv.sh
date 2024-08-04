a0138e8c2a025c5eee11a0f1d2db5d64812ff9de
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-08-05 05:06:50 +0530

    Update crv.sh

--- crv.sh
+++ crv.sh
@@ -1,15 +1,9 @@
-# #!/bin/bash
-
-# BLUE='\033[0;34m'
-# RED='\033[0;31m'
-# NC='\033[0m'
-
-# #!/bin/bash
+#!/bin/bash
 
 # Colors
 RED='\033[0;31m'
+BLUE='\033[0;34m'
 GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Function to display section dividers
@@ -74,60 +68,31 @@ apply_patches() {
     echo -e "${GREEN}Patches applied successfully to $directory.${NC}\n"
 }
 
+# Set up environment
+rm -rf .repo/local_manifest
+repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs
+/opt/crave/resync.sh
+
 # Remove unwanted directories and show status
 divider
-delete_directory "hardware/st/nfc"
-delete_directory "packages/apps/Dialer"
-delete_directory "packages/apps/Contacts"
-delete_directory "packages/apps/Messaging"
-delete_directory "packages/resources/devicesettings"
+delete_directory "device/xiaomi/spes"
+delete_directory "vendor/xiaomi/spes"
+delete_directory "kernel/xiaomi/spes"
+delete_directory "hardware/xiaomi"
+delete_directory "hardware/lineage/compat"
 
 # Device repositories
 divider
-clone_repo "https://github.com/sakshiagrwal/device_xiaomi_spes.git" "thirteen" "device/xiaomi/spes"
-clone_repo "https://github.com/sakshiagrwal/device_xiaomi_sm6225-common.git" "thirteen" "device/xiaomi/sm6225-common"
-clone_repo "https://github.com/PixelExperience-Devices/device_xiaomi_sm6225-common-miuicamera.git" "thirteen" "device/xiaomi/sm6225-common-miuicamera"
-
-clone_repo "https://github.com/PixelExperience-Devices/kernel_xiaomi_sm6225.git" "thirteen" "kernel/xiaomi/sm6225"
-clone_repo "https://github.com/PixelExperience-Devices/device_xiaomi_spes-kernel.git" "thirteen" "device/xiaomi/spes-kernel"
-
-clone_repo "https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_spes.git" "thirteen" "vendor/xiaomi/spes"
-clone_repo "https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sm6225-common.git" "thirteen" "vendor/xiaomi/sm6225-common"
-clone_repo "https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sm6225-common-miuicamera.git" "thirteen" "vendor/xiaomi/sm6225-common-miuicamera"
-
-# Packages repositories
-clone_repo "https://github.com/P-404/android_packages_apps_Dialer.git" "tokui" "packages/apps/Dialer"
-clone_repo "https://github.com/P-404/android_packages_apps_Contacts.git" "tokui" "packages/apps/Contacts"
-clone_repo "https://github.com/P-404/android_packages_apps_Messaging.git" "tokui" "packages/apps/Messaging"
-clone_repo "https://github.com/PixelExperience/packages_resources_devicesettings.git" "thirteen" "packages/resources/devicesettings"
+clone_repo "https://github.com/Kajal4414/android_device_xiaomi_spes.git" "13.0" "device/xiaomi/spes"
+clone_repo "https://github.com/Kajal4414/android_vendor_xiaomi_spes.git" "13.0" "vendor/xiaomi/spes"
+clone_repo "https://github.com/Kajal4414/android_kernel_xiaomi_spes.git" "13.0" "kernel/xiaomi/spes"
 
 # Hardware repositories
-clone_repo "https://github.com/PixelExperience/hardware_st_nfc.git" "thirteen" "hardware/st/nfc"
-clone_repo "https://github.com/PixelExperience/hardware_xiaomi.git" "thirteen" "hardware/xiaomi"
-
-clone_repo "https://github.com/PixelExperience/hardware_qcom-caf_bengal_gps.git" "thirteen" "hardware/qcom-caf/bengal/gps"
-clone_repo "https://github.com/PixelExperience/hardware_qcom-caf_bengal_media.git" "thirteen" "hardware/qcom-caf/bengal/media"
-clone_repo "https://github.com/PixelExperience/hardware_qcom-caf_bengal_audio.git" "thirteen" "hardware/qcom-caf/bengal/audio"
-clone_repo "https://github.com/PixelExperience/hardware_qcom-caf_bengal_display.git" "thirteen" "hardware/qcom-caf/bengal/display"
-
-# Device (QCOM) repositories
-clone_repo "https://github.com/PixelExperience-Devices/device_qcom_common.git" "thirteen" "device/qcom/common"
-clone_repo "https://github.com/PixelExperience-Devices/device_qcom_qssi.git" "thirteen" "device/qcom/qssi"
-clone_repo "https://github.com/PixelExperience-Devices/device_qcom_wlan.git" "thirteen" "device/qcom/wlan"
-clone_repo "https://github.com/PixelExperience-Devices/device_qcom_common-sepolicy.git" "thirteen" "device/qcom/common-sepolicy"
-clone_repo "https://github.com/PixelExperience-Devices/device_qcom_vendor-common.git" "thirteen" "device/qcom/vendor-common"
-
-# Vendor (QCOM) repositories
-clone_repo "https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_qcom_common.git" "thirteen" "vendor/qcom/common"
-clone_repo "https://github.com/PixelExperience/vendor_qcom_opensource_core-utils.git" "thirteen" "vendor/qcom/opensource/core-utils"
-clone_repo "https://github.com/PixelExperience/vendor_qcom_opensource_commonsys_dpm.git" "thirteen" "vendor/qcom/opensource/commonsys/dpm"
-clone_repo "https://github.com/PixelExperience/vendor_qcom_opensource_commonsys-intf_bluetooth.git" "thirteen" "vendor/qcom/opensource/commonsys-intf/bluetooth"
+clone_repo "https://github.com/Kajal4414/android_hardware_xiaomi.git" "13.0" "hardware/xiaomi"
+clone_repo "https://github.com/Kajal4414/android_hardware_lineage_compat.git" "13.0" "hardware/lineage/compat"
 
 # Applying patches
 divider
-apply_patches "frameworks/base" "https://github.com/parixxshit/device_qcom_common.git" "13" "8b318b0" "50ab912"
-apply_patches "device/qcom/common" "https://github.com/parixxshit/device_qcom_common.git" "13" "feb9d85"
-apply_patches "device/qcom/common-sepolicy" "https://github.com/parixxshit/device_qcom_common-sepolicy.git" "13" "e4c9045"
-apply_patches "packages/apps/Settings" "https://github.com/parixxshit/packages_apps_Settings.git" "13" "0631144" "be06b4e"
-apply_patches "vendor/derp" "https://github.com/parixxshit/vendor_derp.git" "13" "01370e0" "33d01f6"
-apply_patches "vendor/qcom/opensource/fm-commonsys" "https://github.com/PixelExperience/vendor_qcom_opensource_fm-commonsys.git" "13" "74f4211"
+apply_patches "packages/apps/crDroidSettings" "https://github.com/Kajal4414/android_packages_apps_crDroidSettings.git" "13.0" "252a9a9" "428bf1e" # Remove GameSpace shortcut and Updater.
+apply_patches "frameworks/base" "https://github.com/Kajal4414/android_frameworks_base.git" "13.0" "d264ce2" "3c2cc04" # Remove support for GameSpace.
+apply_patches "vendor/lineage" "https://github.com/Kajal4414/android_vendor_crdroid.git" "13.0" "0e584f7" # Remove some packages: Eleven, ExactCalculator, GameSpace, Jelly, LineageSetupWizard, MatLog, Recorder and Updater.
