0db9030fc7d35f582fdab6ea99e154f147186576
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-08-05 01:34:10 +0530

    Update crv.sh

--- crv.sh
+++ crv.sh
@@ -13,16 +13,11 @@ git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_hardware_xiaomi
 git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_hardware_lineage_compat.git hardware/lineage/compat
 
 git config --global user.name "Kajal4414" && git config --global user.email "81718060+Kajal4414@users.noreply.github.com"
-(cd packages/apps/crDroidSettings && git log --oneline -10)
-(cd frameworks/base && git log --oneline -10)
-(cd vendor/lineage && git log --oneline -10)
-rm -rf packages/apps/crDroidSettings && git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_packages_apps_crDroidSettings.git packages/apps/crDroidSettings # Remove GameSpace shortcut and Updater.
-rm -rf frameworks/base && git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_frameworks_base.git frameworks/base # Remove support for GameSpace.
-rm -rf vendor/lineage && git clone -b 13.0 --depth 1 https://github.com/Kajal4414/android_vendor_crdroid.git vendor/lineage # Remove some packages: LineageSetupWizard, Updater, Eleven, Recorder, ExactCalculator, Jelly, GameSpace and MatLog.
-
-# (cd device/xiaomi/spes && git pull && git cherry-pick eb06bec^..52a728e) # skiaglthreaded to reorganize files
-# (cd vendor/xiaomi/spes && git pull && git reset --hard HEAD~1)
-# (cd hardware/qcom-caf/sm8250/display && git fetch https://github.com/LineageOS/android_hardware_qcom_display.git lineage-21.0-caf-sm8250 && git cherry-pick 21d3641)
+# (cd device/xiaomi/spes && git fetch https://github.com/Kajal4414/android_device_xiaomi_spes.git 13.0-new && git cherry-pick 8bbcff8) # Idk why.
+(cd packages/apps/crDroidSettings && git fetch https://github.com/Kajal4414/android_packages_apps_crDroidSettings.git && git cherry-pick 252a9a9 428bf1e) # Remove GameSpace shortcut and Updater.
+(cd frameworks/base && git fetch https://github.com/Kajal4414/android_frameworks_base.git && git cherry-pick d264ce2 3c2cc04) # Remove support for GameSpace.
+(cd vendor/lineage && git fetch https://github.com/Kajal4414/android_vendor_crdroid.git && git cherry-pick 0e584f7) # Remove some packages: LineageSetupWizard, Updater, Eleven, Recorder, ExactCalculator, Jelly, GameSpace and MatLog.
+# (cd hardware/qcom-caf/sm8250/display && git fetch https://github.com/LineageOS/android_hardware_qcom_display.git lineage-21.0-caf-sm8250 && git cherry-pick 21d3641) # Remove clang property.
 
 . build/envsetup.sh
 lunch lineage_spes-user
